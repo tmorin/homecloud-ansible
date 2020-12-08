b3093ce6ca9b46e4c7ac72e8a06d67fff8f70b23
tmorin <thibault@morin.io>
2020-12-08 10:07:01 +0100

    chore: move the release process to GitHub Actions [skip-ci]

---
+++ scripts/postbump.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const YAML = require('yaml')
+const pkg = require('../package.json');
+const dstPath = path.join(__dirname, '..', 'galaxy.yml')
+const dstContent = YAML.parse(fs.readFileSync(dstPath, 'utf8'));
+console.log(dstPath, 'from', dstContent.version, 'to', pkg.version);
+dstContent.version = pkg.version;
+fs.writeFileSync(dstPath, YAML.stringify(dstContent));
--- scripts/release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x -e
-rm -Rf playbooks/images/*
-rm -Rf playbooks/vault/*
-rm -Rf tmp/*
-rm -Rf *.gz
-ansible-galaxy collection build --force
