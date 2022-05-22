e1c831006bcc91b246d5f393ace276fa6133f0f8
tmorin <thibault@morin.io>
2022-05-22 13:22:57 +0200

    chore(release): 2.7.0-alpha.3

--- scripts/publish-alpha.sh
+++ scripts/publish-alpha.sh
@@ -6,6 +6,6 @@ npm run release:alpha
 
 rm -f collection/tmorin-homecloud-*.tar.gz
 
-cd collection && ansible-galaxy collection build --force
-
-cd collection && ansible-galaxy collection publish --token "${ANSIBLE_GALAXY_API_KEY}" tmorin-homecloud-*.tar.gz
+cd collection \
+  && ansible-galaxy collection build --force \
+  && ansible-galaxy collection publish --token "${ANSIBLE_GALAXY_API_KEY}" tmorin-homecloud-*.tar.gz
